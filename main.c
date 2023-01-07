8f3a43944ad872907a5f938970ccb5fbc0f12e86
Peter Delevoryas <pdel@meta.com>
2023-01-07 12:17:43 -0800

    riscv: Switch to bitfields

--- main.c
+++ main.c
@@ -1,58 +1,48 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#define MASK8(shift, length)                                                \
-    ((0xff >> (8 - length)) << shift)
+struct Uart {
+    union {
+        uint8_t rhr;
+        uint8_t thr;
+    };
+    uint8_t ier;
+    union {
+        uint8_t isr;
+        struct {
+            uint8_t fifo_enable : 1;
+            uint8_t rx_fifo_reset : 1;
+            uint8_t tx_fifo_reset : 1;
+            uint8_t dma_mode : 1;
+            uint8_t enable_dma_end : 1;
+            uint8_t _0 : 1;
+            uint8_t rx_fifo_trigger_level : 2;
+        } fcr;
+    };
+    uint8_t lcr;
+    uint8_t mcr;
+    struct {
+        uint8_t data_ready : 1;
+        uint8_t overrun_error : 1;
+        uint8_t parity_error : 1;
+        uint8_t framing_error : 1;
+        uint8_t break_interrupt : 1;
+        uint8_t thr_empty : 1;
+        uint8_t transmitter_empty : 1;
+        uint8_t fifo_data_error : 1;
+    } lsr;
+    uint8_t msr;
+    uint8_t spr;
+};
 
-#define REG(reg, addr)                                                      \
-    enum {                                                                  \
-        reg ## _ADDR = (addr),                                              \
-    }
-
-#define FIELD(reg, field, shift, length)                                    \
-    enum {                                                                  \
-        reg ## _ ## field ## _SHIFT = (shift),                              \
-        reg ## _ ## field ## _LENGTH = (length),                            \
-        reg ## _ ## field ## _MASK8 = MASK8(shift, length),                 \
-    }
-
-REG(UART0_RHR, 0x10000000);
-REG(UART0_THR, 0x10000000);
-REG(UART0_ISR, 0x10000002);
-REG(UART0_FCR, 0x10000002);
-REG(UART0_LSR, 0x10000005);
-FIELD(UART0_RHR, RX_CHAR, 0, 8);
-FIELD(UART0_THR, TX_CHAR, 0, 8);
-FIELD(UART0_FCR, FIFO_ENABLE, 0, 1);
-FIELD(UART0_LSR, DATA_READY, 0, 1);
-FIELD(UART0_LSR, THR_EMPTY, 5, 1);
-FIELD(UART0_LSR, TRANSMITTER_EMPTY, 6, 1);
-
-#define lb(reg, field) ({                                                   \
-    uint8_t r = *((volatile uint8_t *)reg ## _ADDR);                        \
-    uint8_t mask = reg ## _ ## field ## _MASK8;                             \
-    uint8_t shift = reg ## _ ## field ## _SHIFT;                            \
-    r &= mask;                                                              \
-    r >>= shift;                                                            \
-    r;                                                                      \
-})
-
-#define sb(reg, field, data) ({                                             \
-    volatile uint8_t *ptr = (volatile uint8_t *)reg ## _ADDR;               \
-    uint8_t mask = reg ## _ ## field ## _MASK8;                             \
-    uint8_t shift = reg ## _ ## field ## _SHIFT;                            \
-    uint8_t r = *ptr;                                                       \
-    r &= ~mask;                                                             \
-    r |= data << shift;                                                     \
-    *ptr = r;                                                               \
-})
+volatile struct Uart *UART = (volatile struct Uart *)0x10000000;
 
 void putchar(char c)
 {
-    while (!lb(UART0_LSR, THR_EMPTY)) {
+    while (!UART[0].lsr.thr_empty) {
         // wait
     }
-    sb(UART0_THR, TX_CHAR, c);
+    UART[0].thr = c;
 
     if (c == '\r') {
         putchar('\n');
@@ -61,10 +51,10 @@ void putchar(char c)
 
 char getchar(void)
 {
-    while (!lb(UART0_LSR, DATA_READY)) {
+    while (!UART[0].lsr.data_ready) {
         // wait
     }
-    return lb(UART0_RHR, RX_CHAR);
+    return UART[0].rhr;
 }
 
 void print(const char *s)
@@ -77,7 +67,7 @@ void print(const char *s)
 
 void main(void)
 {
-    sb(UART0_FCR, FIFO_ENABLE, 1);
+    UART[0].fcr.fifo_enable = 1;
 
     print("hello world\n");
 
